1723c7ed1ba256ee8a9e440c41794b1cf20e3e78
Buğra Demir <bugraademirr2004@gmail.com>
2026-08-07 10:44:30 +0300

    a

--- rezervasyon_frontend/src/app/(resepsiyon)/layout.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import ResepsiyonShell from "@/components/resepsiyon/ResepsiyonShell";
-
-export default function ResepsiyonLayout({ children }: { children: React.ReactNode }) {
-  return <ResepsiyonShell>{children}</ResepsiyonShell>;
-}
